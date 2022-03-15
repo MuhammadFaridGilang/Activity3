03c4127ed2115c2df66cf77c99b9093121b72885
Muhammad Farid Gilang Nuswantoro <m.farid.ft20@mail.umy.ac.id>
2022-03-15 14:48:19 +0700

    Activity 4 | Membuat menu dan Floating Button pada android

---
+++ app/src/main/java/com/example/activity1/ActivityHasil.java
@@ -0,0 +1,40 @@
+package com.example.activity1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ActivityHasil extends AppCompatActivity {
+    //Mendeklarasikan variabel dengan tipe data TextView
+    TextView txemail, txpassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil);
+
+        //Menghubungkan variabel txemail dengan komponen  Textview pada layout
+        txemail = findViewById(R.id.tvEmail);
+        //Menghubungkan variabel txpassword dengan komponen  Textview pada layout
+        txpassword = findViewById(R.id.tvPassword);
+
+        //mendeklarasikan variabel bundle yang akan digunakan untuk mengambil
+        //pesan yang dikirimkan melalui method intent
+        Bundle bundle = getIntent().getExtras();
+
+        //membuat variabel string yang digunakan untuk menyimpan data yang
+        //dikirimkan dari activity sebelumnya dengan kunci "a"
+        String email = bundle.getString("a");
+
+        //Membuat variabel string yang digunakan untuk menyimpan data yang
+        // dikirimkan dari activity sebelumnya dengan kunci "b"
+        String pass = bundle.getString("b");
+
+        //Menampilkan value dari variabel email kedalam txemail
+        txemail.setText(email);
+
+        //Menampilkan value dari variabel email kedalam txpassword
+        txpassword.setText(pass);
+    }
+}
--- app/src/main/java/com/example/activity1/MainActivity.java
+++ app/src/main/java/com/example/activity1/MainActivity.java
@@ -1,14 +1,134 @@
 package com.example.activity1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Deklarasi variabel untuk button
+    Button btnLogin;
+
+    //Deklarasi variabel untuk EditText
+    EditText edemail, edpassword;
+
+    //Deklarasi variabel untuk menyimpan email dan password
+    String nama, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Menghubungkan variabel btnLogin dengan componen button pada layout
+        btnLogin=findViewById(R.id.btSignin);
+
+        //menghubungkan variabel edemail dengan komponen button pada layout
+        edemail=findViewById(R.id.edEmail);
+
+        //menghubungkan variabel edpassword dengan komponen button pada layout
+        edpassword=findViewById(R.id.edPassword);
+
+        //membuat fungsi onclick pada button btnLogin
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                //menyimpan input user di edittext email kedalam variabel nama
+                nama = edemail.getText().toString();
+
+                //menyimpan input user di edittext password kedalam variabel password
+                password = edpassword.getText().toString();
+
+                //membuat variabel toast dan membuat toast dengan menambahkan variabel nama dan password
+                //Toast t = Toast.makeText(getApplicationContext(),
+                        //"email anda: " + nama + "dan password anda: " + password + "", Toast.LENGTH_LONG);
+
+                //menampilkan toast
+                    //t.show();
+
+                //mengeset email yang benar
+                String email = "admin@mail.com";
+
+                //mengeset password yang benar
+                String pass = "123";
+
+                //mengecek apakah edittext email dan password terdapat isi atau tidak
+                if ( nama.isEmpty() || password.isEmpty()){
+                    //membuat variabel toast dan menampilkan pesan "edittext tidak boleh kosong"
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Email dan password wajib diisi!",
+                            Toast.LENGTH_LONG);
+                    //menampilkan toast
+                    t.show();
+                } else {
+                    //mengecek apakah isi dari email dan password sudah sama dengan email dan password yang sudah diset
+                    if (nama.equals(email) && password.equals(pass)) {
+                        //membuat variabel toast dan menampilkan pesan "login sukses"
+                        Toast t = Toast.makeText(getApplicationContext(),
+                                "Login  Sukses",
+                                Toast.LENGTH_LONG);
+                        //menampilkan toast
+                        t.show();
+
+                        //membuat objek bundle
+                        Bundle b = new Bundle();
+
+                        //memasukkan value dari variabel password dengan kunci "b"
+                        //dan dimasukkan kedalam bundle
+                        b.putString("a", nama.trim());
+
+                        //memasukkan value dari variabel password dengan kuncu "b"
+                        //dan dimasukkan kedalam bundle
+                        b.putString("b", password.trim());
+
+                        //membuat objek intent berpindah activity dari mainactivity ke Activity Hasil
+                        Intent i = new Intent(getApplicationContext(), ActivityHasil.class);
+
+                        //memasukkan bundle kedalam intent dengan dikirimkan ke ActivityHasil
+                        i.putExtras(b);
+
+                        //berpindah ke ActivityHasil
+                        startActivity(i);
+                    }else{
+                        //memasukkan bundle kedalam intent dan menampilkan pesan "login gagal"
+                        Toast t =Toast.makeText(getApplicationContext(),
+                                "Login Gagal", Toast.LENGTH_LONG);
+                        //menampilkan toast
+                        t.show();
+                    }
+                }
+
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Method untuk menampilkan menu
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        //Membuat kondisi jika yang dipilih adalah id mnDaftar
+        if (item.getItemId() == R.id.mnDaftar)
+        {
+            //Method untuk memanggil activity "DaftarActivity"
+            Intent i = new Intent(getApplicationContext(), ActivityDaftar.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
